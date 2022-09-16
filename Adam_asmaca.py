8208abf6515a4b394a87eb54c7fd5f9fc35515da
Dr-mnu23 <96422181+Dr-mnu23@users.noreply.github.com>
2022-09-16 16:38:52 +0300

    Create Adam_asmaca.py

---
+++ Adam_asmaca.py
@@ -0,0 +1,127 @@
+# klasik adam asmaca oyunu
+import random
+HANGMAN_PICS = ['''
+  +---+
+      |
+      |
+      |
+     ===''', '''
+  +---+
+  O   |
+      |
+      |
+     ===''', '''
+  +---+
+  O   |a
+  |   |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|   |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+ /    |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+ / \  |
+     ===''']
+words = 'elma sinek masa dergi mustafa'.split()
+
+def getRandomWord(wordList):
+    # This function returns a random string from the passed list of strings.
+    wordIndex = random.randint(0, len(wordList) - 1)
+    return wordList[wordIndex]
+
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_PICS[len(missedLetters)])
+    print()
+
+    print('Yanlış harfler:', end=' ')
+    for letter in missedLetters:
+        print(letter, end=' ')
+    print()
+
+    blanks = '_' * len(secretWord)
+
+    for i in range(len(secretWord)): # replace blanks with correctly guessed letters
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks: # show the secret word with spaces in between each letter
+        print(letter, end=' ')
+    print()
+
+def getGuess(alreadyGuessed):
+    # Returns the letter the player entered. This function makes sure the player entered a single letter and not something else.
+    while True:
+        print('Bir harf söyle.')
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('lütfen tek harf gir.')
+        elif guess in alreadyGuessed:
+            print('Bu harfi zaten söyledin. Başka harf söyle lütfen.')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Lütfen sadece harf söyle.')
+        else:
+            return guess
+
+def playAgain():
+    # This function returns True if the player wants to play again; otherwise, it returns False.
+    print('Tekrar oynamak ister misin? (Evet / Hayır)')
+    return input().lower().startswith('e')
+
+
+print('A D A M    A S M A C A')
+missedLetters = ''
+correctLetters = ''
+secretWord = getRandomWord(words)
+gameIsDone = False
+
+while True:
+    displayBoard(missedLetters, correctLetters, secretWord)
+
+    # Let the player enter a letter.
+    guess = getGuess(missedLetters + correctLetters)
+
+    if guess in secretWord:
+        correctLetters = correctLetters + guess
+
+        # Check if the player has won.
+        foundAllLetters = True
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetters = False
+                break
+        if foundAllLetters:
+            print('Doğru! Tıttuğum kelime "' + secretWord + '"! kazandın!')
+            gameIsDone = True
+    else:
+        missedLetters = missedLetters + guess
+
+        # Check if player has guessed too many times and lost.
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
+            displayBoard(missedLetters, correctLetters, secretWord)
+            print('Tahmin hakkın bitti!\n ' + str(len(missedLetters)) + ' tane yanlı tahmin ve ' + str(len(correctLetters)) + ' tane doğru tahmin yaptın. Tuttuğum kelime "' + secretWord + '"')
+            gameIsDone = True
+
+    # Ask the player if they want to play again (but only if the game is done).
+    if gameIsDone:
+        if playAgain():
+            missedLetters = ''
+            correctLetters = ''
+            gameIsDone = False
+            secretWord = getRandomWord(words)
+        else:
+            break
